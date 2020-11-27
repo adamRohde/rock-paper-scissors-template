675b9412c142898cccd667b7bdb35f8cf3929ca2
Adam Rohde <adamrohde@outlook.com>
2020-11-27 15:04:11 -0600

    layout

--- res/js/theme.js
+++ res/js/theme.js
@@ -1,22 +1,22 @@
-let root = document.querySelector(":root");
+// let root = document.querySelector(":root");
 
-document.getElementById("dark-mode").onclick = function () {
-    root.style.setProperty("--theme-color-nav", "rgb(73, 73, 73)");
-    root.style.setProperty("--theme-color-back", "#131313");
-    root.style.setProperty("--theme-color-keys", "#0E4F9E");
-    root.style.setProperty("--theme-color-keys-secondary", "#8FB3E0");
-    root.style.setProperty("--theme-color-text", "rgb(255, 255, 255)");
-    root.style.setProperty("--theme-color-burger", "rgb(255, 255, 255)");
-};
+// document.getElementById("dark-mode").onclick = function () {
+//     root.style.setProperty("--theme-color-nav", "rgb(73, 73, 73)");
+//     root.style.setProperty("--theme-color-back", "#131313");
+//     root.style.setProperty("--theme-color-keys", "#0E4F9E");
+//     root.style.setProperty("--theme-color-keys-secondary", "#8FB3E0");
+//     root.style.setProperty("--theme-color-text", "rgb(255, 255, 255)");
+//     root.style.setProperty("--theme-color-burger", "rgb(255, 255, 255)");
+// };
 
-document.getElementById("light-mode").onclick = function () {
-    root.style.setProperty("--theme-color-nav", "rgb(213, 213, 213)");
-    root.style.setProperty("--theme-color-back", "rgb(255, 255, 255)");
-    root.style.setProperty("--theme-color-keys", "rgb(220, 220, 220)");
-    root.style.setProperty("--theme-color-keys-secondary", "rgb(191, 191, 191)");
-    root.style.setProperty("--theme-color-text", "rgb(0, 0, 0)");
-    root.style.setProperty("--theme-color-burger", "rgb(64, 64, 64)");
-};
+// document.getElementById("light-mode").onclick = function () {
+//     root.style.setProperty("--theme-color-nav", "rgb(213, 213, 213)");
+//     root.style.setProperty("--theme-color-back", "rgb(255, 255, 255)");
+//     root.style.setProperty("--theme-color-keys", "rgb(220, 220, 220)");
+//     root.style.setProperty("--theme-color-keys-secondary", "rgb(191, 191, 191)");
+//     root.style.setProperty("--theme-color-text", "rgb(0, 0, 0)");
+//     root.style.setProperty("--theme-color-burger", "rgb(64, 64, 64)");
+// };
 
 // --theme-color-nav: #494949;
 //     --theme-color-back: #131313;
